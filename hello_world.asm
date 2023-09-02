88cefba5985a27a4da004d02d1b6d0d456daba1a
yassinebenaid <yassinebenaide3@gmail.com>
2023-09-02 19:12:10 +0100

    added my name to the greeting

--- hello_world.asm
+++ hello_world.asm
@@ -1,14 +1,23 @@
 section .data
-    msg db      "hello, world!"
+    greeting db "Hi mr. : " 
+    name db "yassinebenaid" ; my beautifull name
 
 section .text
     global _start
+
 _start:
-    mov     rax, 1
-    mov     rdi, 1
-    mov     rsi, msg
-    mov     rdx, 13
+    mov rax, 1          
+    mov rdi, 1          
+    mov rsi, greeting   
+    mov rdx, 9
     syscall
-    mov    rax, 60
-    mov    rdi, 0
+
+    mov rax , 1
+    mov rdi , 1
+    mov esi , name
+    mov rdx , 13
+    syscall
+
+    mov rax , 60
+    mov rdi , 0 
     syscall
